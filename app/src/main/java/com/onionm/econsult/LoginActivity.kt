b35cc300d64cd527e91e5673686b10fe5d23a4b6
isonz <isonz@qq.com>
2018-08-11 12:55:42 +0800

    asdf

--- app/src/main/java/com/onionm/econsult/LoginActivity.kt
+++ app/src/main/java/com/onionm/econsult/LoginActivity.kt
@@ -1,17 +1,48 @@
 package com.onionm.econsult
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
+import android.view.WindowManager
+import com.safframework.log.L
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity()
 {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        L.i("ON CREATE")
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        //透明系统状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        }
+
         setContentView(R.layout.activity_login)
 
+        login_by_username.setOnClickListener{
+            startActivity(Intent(LoginActivity@this, LoginByUsernameActivity::class.java))
+        }
+
+        weixin_icon.setOnClickListener{
+            startActivity(Intent(LoginActivity@this, MainActivity::class.java))
+        }
+    }
+
+    override fun onDestroy() {
+        L.i("ON destroy")
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        L.i("ON start")
+        super.onStart()
     }
 
     // 包裹范围内 属于静态方法
---
+++ app/src/main/java/com/onionm/econsult/LoginByUsernameActivity.kt
@@ -0,0 +1,30 @@
+package com.onionm.econsult
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login_username.*
+
+class LoginByUsernameActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_username)
+
+        submit_login.setOnClickListener {
+            startActivity(Intent(LoginByUsernameActivity@ this, MainActivity::class.java))
+        }
+
+    }
+
+    // 包裹范围内 属于静态方法
+    companion object {
+        //
+    }
+
+
+
+}
