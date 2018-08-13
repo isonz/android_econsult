fb460026290ac155041cb21af834fdd5f65a5363
isonz <isonz@qq.com>
2018-08-13 18:58:29 +0800

    asdf

--- app/src/main/java/com/onionm/econsult/MainActivity.kt
+++ app/src/main/java/com/onionm/econsult/MainActivity.kt
@@ -3,30 +3,33 @@ package com.onionm.econsult
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.menu.MenuBuilder
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.navigation.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_inquiry -> {
-                message.setText(R.string.title_inquiry)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_message -> {
-                message.setText(R.string.title_message)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_discovery -> {
-                message.setText(R.string.title_discovery)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_my -> {
-                message.setText(R.string.title_my)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -37,6 +40,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        // supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        // supportActionBar!!.setDisplayShowTitleEnabled(true)
+        // supportActionBar!!.setHomeButtonEnabled(true)
+
+        navigation_bottom.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu?.let {
+            if (menu is MenuBuilder) {
+                try {
+                    val field = menu.javaClass.getDeclaredField("mOptionalIconsVisible")
+                    field.isAccessible = true
+                    field.setBoolean(menu, true)
+                } catch (ignored: Exception) {
+                    // ignored exception
+                }
+            }
+        }
+        val inflater = menuInflater
+        inflater.inflate(R.menu.actionbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
