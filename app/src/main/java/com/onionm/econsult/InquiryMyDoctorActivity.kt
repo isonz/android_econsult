c6f374576adbaa9a31423bc6add4cdbb4ae6f57e
isonz <isonz@qq.com>
2018-08-23 18:31:41 +0800

    asdfsd

--- app/src/main/java/com/onionm/econsult/InquiryMyDoctorActivity.kt
+++ app/src/main/java/com/onionm/econsult/InquiryMyDoctorActivity.kt
@@ -2,12 +2,10 @@ package com.onionm.econsult
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.view.menu.MenuBuilder
 import android.view.Menu
-import kotlinx.android.synthetic.main.inquiry_select_doctor.*
-import kotlinx.android.synthetic.main.navigation.*
+import kotlinx.android.synthetic.main.inquiry_my_doctor.*
 
 class InquiryMyDoctorActivity : AppCompatActivity()
 {
@@ -16,15 +14,44 @@ class InquiryMyDoctorActivity : AppCompatActivity()
         setContentView(R.layout.inquiry_my_doctor)
 
         inquiry_select_online_doctor_btn_1.setOnClickListener{
-            startActivity(Intent(InquirySelectOnlineDoctorActivity@this, InquirySelectedOnlineDoctorActivity::class.java))
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryVisitActivity::class.java))
         }
         inquiry_select_online_doctor_btn_3.setOnClickListener{
-            startActivity(Intent(InquirySelectOnlineDoctorActivity@this, InquirySelectedOnlineDoctorActivity::class.java))
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryVisit2Activity::class.java))
         }
         inquiry_select_online_doctor_btn_4.setOnClickListener{
-            startActivity(Intent(InquirySelectOnlineDoctorActivity@this, InquirySelectedOnlineDoctorActivity::class.java))
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryVisitActivity::class.java))
         }
 
+
+        inquiry_recommended_doctor_btn_1.setOnClickListener{
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryVisitActivity::class.java))
+        }
+        inquiry_recommended_doctor_btn_3.setOnClickListener{
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryVisit2Activity::class.java))
+        }
+
+
+        inquiry_signing_doctor_btn_1.setOnClickListener{
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryMyDoctorSigningActivity::class.java))
+        }
+        inquiry_signing_doctor_btn_1.setOnClickListener{
+            startActivity(Intent(InquiryMyDoctorActivity@this, InquiryMyDoctorSigningActivity::class.java))
+        }
+
+
+        //======================== TAB
+        tabHost.setup()
+        var spec = tabHost.newTabSpec("推荐医生")
+        spec.setContent(tab1.id)
+        spec.setIndicator("推荐医生")
+        tabHost.addTab(spec)
+
+        spec = tabHost.newTabSpec("已签约医生")
+        spec.setContent(tab2.id)
+        spec.setIndicator("已签约医生")
+        tabHost.addTab(spec)
+        //======================== TAB
     }
 
 
